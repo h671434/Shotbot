475cf028cb6e777c203f0c8ddd1591a355a41541
h671434 <111515930+h671434@users.noreply.github.com>
2023-01-11 16:20:30 +0100

    Delete run-bot.bat

--- run-bot.bat
+++
@@ -1,7 +0,0 @@
-@rem Change the working directory to the location of this file so that relative paths will work
-cd C:\Users\Aleksander XXVI\Documents\RLBot\Shotbot
-
-@rem Start running the bot.
-call ./gradlew.bat --stacktrace --no-daemon run 
-
-pause
